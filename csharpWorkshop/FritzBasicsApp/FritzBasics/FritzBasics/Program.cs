1bb1c70fbead6265ddfe067718eb7680f9c8b650
Sarah Dutkiewicz <sarah@cletechconsulting.com>
2018-05-18 09:26:00 -0400

    Starting template for the C# Workshop Basics section with Sadukie

---
+++ csharpWorkshop/FritzBasicsApp/FritzBasics/FritzBasics/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FritzBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FritzApp myApp = new FritzApp();
+            myApp.Run();
+        }
+    }
+}
